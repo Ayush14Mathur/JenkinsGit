03b8f129deffe5ca4d7310cc5416c5ee4cf2d67c
Ayush14Mathur <128884030+Ayush14Mathur@users.noreply.github.com>
2023-04-26 11:28:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello {
 public static void main (String[] args) {
 System.out.println("Hello World");
   System.out.println("Welcome to Jenkins");
+  System.out.println("End of program");
 }
 }
